2fd730b08f2eda9af934728803e2ee6b61ff4c82
Arsen Danilov <rsndanilov@yandex.ru>
2024-11-06 11:14:54 +0300

    Work with router settings

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,19 +5,57 @@ import { ThemeContextProvider } from "../theme-context/theme-context";
 import { UserContextProvider } from "../user-context/user-context";
 import { Provider } from "react-redux";
 import { store } from "../../redux/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 export const ThemeContext = createContext();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "restaurant",
+        element: <RestaurantsPage title={"Cuisines"} />,
+        children: [
+          {
+            path: ":restaurantId",
+            element: <div>123</div>,
+          },
+        ],
+      },
+      {
+        path: "/about",
+        element: <div>about</div>,
+      },
+    ],
+  },
+]);
 
 export const App = ({ title }) => {
   return (
     <div>
+      {/* <RouterProvider router={router}>
+        <Provider store={store}>
+          <ThemeContextProvider>
+            <UserContextProvider>
+              <Layout>
+                <h1>{title}</h1>
+                <RestaurantsPage title={"Cuisines"} />
+              </Layout>
+            </UserContextProvider>
+          </ThemeContextProvider>
+        </Provider>
+      </RouterProvider> */}
       <Provider store={store}>
         <ThemeContextProvider>
           <UserContextProvider>
-            <Layout>
-              <h1>{title}</h1>
-              <RestaurantsPage title={"Cuisines"} />
-            </Layout>
+            <RouterProvider router={router}>
+              <Layout>
+                <h1>{title}</h1>
+                <RestaurantsPage title={"Cuisines"} />
+              </Layout>
+            </RouterProvider>
           </UserContextProvider>
         </ThemeContextProvider>
       </Provider>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,14 +3,15 @@ import { Footer } from "../Footer/Footer";
 import { ScrollBar } from "../ScrollBar/ScrollBar";
 import { ToggleThemeButton } from "../Toggle-theme-button/Toggle-theme-button";
 import { Cart } from "../Cart/Cart";
+import { Outlet } from "react-router-dom";
 
-export const Layout = ({ children }) => {
+export const Layout = () => {
   return (
     <div>
       <ScrollBar />
       <ToggleThemeButton />
       <Header />
-      {children}
+      <Outlet />
       <Cart />
       <Footer />
     </div>
