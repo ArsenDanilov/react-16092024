a9f4466724b9d6656bdea4f4ea90b1965f91e030
Arsen Danilov <rsndanilov@yandex.ru>
2024-11-20 15:47:32 +0300

    Организована загрузка данных ресторанов с сервера

--- src/redux/Restaurants/index.js
+++ src/redux/Restaurants/index.js
@@ -1,22 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { normalizedRestaurants } from "../../../materials/normalized-mock";
+import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import { getRestaurants } from "./get-restaurants";
 
-const initialState = {
-    entities: normalizedRestaurants.reduce((acc, item) => {
-        acc[item.id] = item;
+// const initialState = {
+//     entities: normalizedRestaurants.reduce((acc, item) => {
+//         acc[item.id] = item;
 
-        return acc;
-    }, {}), // создаем объект, в котором ключами будут айдишники, а значениями - сущности 
-    ids: normalizedRestaurants.map(({ id }) => id), // создаем массив айдишников сущностей 
-}
+//         return acc;
+//     }, {}), // создаем объект, в котором ключами будут айдишники, а значениями - сущности 
+//     ids: normalizedRestaurants.map(({ id }) => id), // создаем массив айдишников сущностей 
+// }
+
+
+// const initialState = {
+//     entities: {},
+//     ids: [],
+//     requestStatus: "idle"
+// }
+
+const entityAdapter = createEntityAdapter();
 
 export const restaurantsSlice = createSlice({
     name: "restaurants",
-    initialState,
+    initialState: entityAdapter.getInitialState({ requestStatus: "idle" }),
     selectors: { // селектор - функция, которая выбирает небольшой кусочек этих данных
         selectRestaurantsIds: (state) => state.ids,
-        selectRestaurantById: (state, id) => state.entities[id]
+        selectRestaurantById: (state, id) => state.entities[id],
+        selectRestaurantsRequestStatus: (state) => state.requestStatus,
     },
+    extraReducers: (builder) => builder
+        .addCase(getRestaurants.pending, (state) => {
+            state.requestStatus = "pending";
+        })
+        .addCase(getRestaurants.fulfilled, (state, { payload }) => {
+            // state.entities = payload.reduce((acc, item) => {
+            //     acc[item.id] = item;
+
+            //     return acc;
+            // }, {}),
+            //     state.ids = payload.map(({ id }) => id),
+                entityAdapter.setAll(state, payload);
+                state.requestStatus = "fulfilled";
+        })
+        .addCase(getRestaurants.rejected, (state) => {
+            state.requestStatus = "rejected";
+        }),
 });
 
-export const { selectRestaurantsIds,  selectRestaurantById} = restaurantsSlice.selectors;
+export const { selectRestaurantsIds, selectRestaurantById, selectRestaurantsRequestStatus } = restaurantsSlice.selectors;
