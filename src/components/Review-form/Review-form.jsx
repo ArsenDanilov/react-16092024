bf6b97a454b7123e37c0870bc2487202ec00ceef
Arsen Danilov <rsndanilov@yandex.ru>
2025-01-24 17:52:58 +0300

    Реализован рейтинг в виде звезд

--- src/components/Review-form/Review-form.jsx
+++ src/components/Review-form/Review-form.jsx
@@ -1,52 +1,34 @@
-import { Counter } from "../Counter/Counter";
 import { useForm } from "./use-form";
 import { Button } from "../Button/Button";
 import { useAddReviewMutation } from "../../redux/services/api/api";
 import styles from "./review-form.module.css"
-import buttonStyles from "../Button/button.module.css"
+import { ReviewStars } from "../Review-stars/Review-stars";
 
 export const ReviewForm = ({ restaurantId }) => {
-  const { name, text, rating, setText, setRating, setClear } =
+  const { name, text, rating, setText, setRating } =
     useForm();
-
-  const increaseHandler = () => {
-    if (rating < 5) {
-      setRating(rating + 1);
-    }
-  };
-  const decreaseHandler = () => {
-    if (rating > 0) {
-      setRating(rating - 1);
-    }
-  };
-
 
   const [addReview] = useAddReviewMutation();
 
   return (
     
-    <form className={styles.reviewForm} action="" onReset={() => setClear()} onSubmit={(e) => e.preventDefault()}>
+    <form className={styles.reviewForm} action="" onSubmit={(e) => e.preventDefault()}>
       <h3 className={styles.reviewForm__title}>Leave your review</h3>
       <div>
         <textarea className={styles.reviewForm__text} name="message" id="" cols="30" rows="3" value={text} onChange={(event) => setText(event.target.value)}></textarea>
       </div>
       <div className={styles.reviewForm__rating}>
         <span>Rating:</span>
-        <Counter
-          value={rating}
-          increase={increaseHandler}
-          decrease={decreaseHandler}
-          className={buttonStyles.button__counter}
-        />
+        <ReviewStars setRating={setRating} rating={rating}/>
       </div>
-      <Button onClick={() => setClear} type={"reset"} text={"Clear"} />
       <Button
         onClick={() => addReview({ restaurantId, review: {
           name, 
           text,
           rating,
         } })}
-        text={"Submit"}
+        text={"PUBLISH REVIEW"}
+        className={styles.reviewForm__submitBtn}
       />
     </form>
   );
