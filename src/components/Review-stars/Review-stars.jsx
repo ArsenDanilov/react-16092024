4ebf04921c34567ff362c03d50c692d9fe669513
Arsen.Danilov <arsen.danilov@ite.group>
2025-01-30 12:12:59 +0300

    Убрана мутация rating в компоненте ReviewStars

--- src/components/Review-stars/Review-stars.jsx
+++ src/components/Review-stars/Review-stars.jsx
@@ -1,30 +1,24 @@
-import { useState } from "react";
 import { ReviewStar } from "../Review-star/Review-star";
 
 export const ReviewStars = ({ setRating, rating }) => {
-  const [selectedStars, setSelectedStars] = useState(0);
-
+  
   const handleStarClick = (index) => {
-    if (selectedStars === index) {
-      setSelectedStars(index - 1);
-    } else {
-      setSelectedStars(index);
-    }
-
-    rating = index;
-
-    setRating(rating);
+    const newRating = rating === index ? index - 1 : index;
+    setRating(newRating);
   };
 
   return (
     <div>
-      {Array.from({ length: 5 }, (_, index) => (
-        <ReviewStar
-          key={index}
-          isSelected={index < selectedStars}
-          onClick={() => handleStarClick(index + 1)}
-        />
-      ))}
+      {Array.from({ length: 5 }, (_, index) => {
+        const starIndex = index + 1;
+        return (
+          <ReviewStar
+            key={starIndex}
+            isSelected={starIndex <= rating}
+            onClick={() => handleStarClick(starIndex)}
+          />
+        );
+      })}
     </div>
   );
 };
